2698552adec11d5e5e87e1ea2cb1cce3608b8339
Mike Bostock <mbostock@gmail.com>
2017-02-07 16:03:11 -0800

    Inline styles before rendering.

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 (function(global) {
-  Array.prototype.forEach.call(global.document.querySelectorAll("svg"), function(svg, i) {
-    var canvas = global.document.createElement("canvas"),
+  var document = global.document,
+      forEach = Array.prototype.forEach,
+      styles = document.querySelectorAll("style");
+
+  forEach.call(document.querySelectorAll("svg"), function(svg, i) {
+    forEach.call(styles, function(style) { svg.appendChild(style.cloneNode(true)); });
+
+    var canvas = document.createElement("canvas"),
         context = canvas.getContext("2d"),
         image = new Image,
         ratio = global.devicePixelRatio || 1,
         width = svg.getAttribute("width") * ratio,
         height = svg.getAttribute("height") * ratio,
         url = URL.createObjectURL(new Blob([(new XMLSerializer).serializeToString(svg)], {type: "image/svg+xml"}));
-    canvas.width = width;
-    canvas.height = height;
-    image.onload = function () {
+
+    image.onload = function() {
       context.drawImage(this, 0, 0, width, height);
       url = URL.revokeObjectURL(url);
       canvas.toBlob(function(blob) {
-        var a = global.document.createElement("a");
+        var a = document.createElement("a");
         a.download = "untitled" + (i ? "-" + i : "") + ".png";
         a.href = url = URL.createObjectURL(blob);
         a.textContent = "untitled-" + i + ".png";
-        global.document.body.appendChild(a);
+        document.body.appendChild(a);
         a.click();
         setTimeout(function() {
           url = URL.revokeObjectURL(url);
-          global.document.body.removeChild(a);
+          document.body.removeChild(a);
         }, 10);
       });
     };
+
+    canvas.width = width;
+    canvas.height = height;
     image.src = url;
   });
 })(this);
