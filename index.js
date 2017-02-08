717627d7f324fedc38c7367036209f13b6e2402a
Mike Bostock <mbostock@gmail.com>
2017-02-08 10:40:03 -0800

    Inline styles.
    Fix #2. Fix #3. Fix #4.

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 (function(global) {
   var document = global.document,
-      forEach = Array.prototype.forEach,
-      styles = document.querySelectorAll("style");
+      body = document.body,
+      forEach = Array.prototype.forEach;
 
-  forEach.call(document.querySelectorAll("svg"), function(svg, i) {
+  function visit(element, clone, callback) {
+    callback(element, clone);
+    if (clone.firstElementChild) visit(element.firstElementChild, clone.firstElementChild, callback);
+    if (clone.nextElementSibling) visit(element.nextElementSibling, clone.nextElementSibling, callback);
+  }
+
+  function inlineStyle(root) {
+    var frame = body.appendChild(document.createElement("iframe")),
+        clone = frame.contentDocument.body.appendChild(root.cloneNode(true));
+
+    visit(root, clone, function(element, clone) {
+      var elementStyle = global.getComputedStyle(element),
+          cloneStyle = frame.contentWindow.getComputedStyle(clone),
+          cloneStyles = clone.getAttribute("style") || "";
+      for (var i = 0, n = elementStyle.length; i < n; ++i) {
+        var name = elementStyle[i],
+            elementValue = elementStyle[name],
+            cloneValue = cloneStyle[name];
+        if (elementValue !== cloneValue) {
+          cloneStyles += ";" + name + ":" + elementValue;
+        }
+      }
+      if (cloneStyles) clone.setAttribute("style", cloneStyles);
+    });
+
+    body.removeChild(frame);
+    return clone;
+  }
+
+  function serialize(root) {
+    return (new XMLSerializer).serializeToString(inlineStyle(root));
+  }
+
+  forEach.call(document.querySelectorAll("svg"), function(svg) {
     if (svg.namespaceURI !== "http://www.w3.org/2000/svg") return; // Not really an SVG.
     if (svg.ownerSVGElement) return; // An SVG within another SVG.
-
-    forEach.call(styles, function(style) { svg.appendChild(style.cloneNode(true)); });
 
     var canvas = document.createElement("canvas"),
         context = canvas.getContext("2d"),
@@ -15,21 +46,20 @@
         ratio = global.devicePixelRatio || 1,
         width = svg.getAttribute("width") * ratio,
         height = svg.getAttribute("height") * ratio,
-        url = URL.createObjectURL(new Blob([(new XMLSerializer).serializeToString(svg)], {type: "image/svg+xml"}));
+        url = URL.createObjectURL(new Blob([serialize(svg)], {type: "image/svg+xml"}));
 
     image.onload = function() {
       context.drawImage(this, 0, 0, width, height);
       url = URL.revokeObjectURL(url);
       canvas.toBlob(function(blob) {
         var a = document.createElement("a");
-        a.download = "untitled" + (i ? "-" + i : "") + ".png";
+        a.download = "untitled.png";
         a.href = url = URL.createObjectURL(blob);
-        a.textContent = "untitled-" + i + ".png";
-        document.body.appendChild(a);
+        body.appendChild(a);
         a.click();
         setTimeout(function() {
           url = URL.revokeObjectURL(url);
-          document.body.removeChild(a);
+          body.removeChild(a);
         }, 10);
       });
     };
